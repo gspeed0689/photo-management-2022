11d2ea771a8fc197479e9a06ea8a771c3dc738dc
Garrett Speed <6378547+gspeed0689@users.noreply.github.com>
2022-01-02 16:04:26 +0100

    Update photo_management.py

--- bin/photo_management.py
+++ bin/photo_management.py
@@ -12,10 +12,15 @@ import secrets
 import uuid
 import hashlib
 import argparse
+import multiprocessing
+import subprocess
 
 class photo_import:
-    def __init__(self):
+    def __init__(self, source_folder, output_folder="default"):
+        self.source_folder = source_folder
+        self.output_folder = output_folder
         pass
+    
 
 class photo:
     def __init__(self, file_path):
@@ -60,6 +65,31 @@ class photo:
         return(uid)
     def load_exif_data(self):
         pass
+
+class video:
+    def __init__(self, file_path, conversion=True, conversion_method="batch"):
+        self.filepath = file_path
+        self.filename = file_path.split(os.sep)[-1]
+        self.filetype = self.filename.split(".")[-1]
+        self.conversion = conversion
+        self.conversion_method = conversion_method
+        pass
+    def conversion_control(self):
+        affinity_prefix = ["start", "/affinity", self.affinity_bin()]
+        if self.conversion == True:
+            if self.conversion_method == "batch":
+                pass
+            elif self.conversion_method == "subproc":
+                pass
+        pass
+    def affinity_bin(self):
+        total_cores = multiprocessing.cpu_count()
+        active_cores = total_cores - config.affinity_reserve_cores
+        active_1s = active_cores * "1"
+        inactive_0s = config.affinity_reserve_cores * "0"
+        bin_str = f"0b{active_1s}{inactive_0s}"
+        hex_str = str(hex(int(bin_str, 2)))[2:]
+        return(hex_str)
 
 def arguments():
     pass
