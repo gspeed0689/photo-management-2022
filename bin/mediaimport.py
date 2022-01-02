7745d3e58ca62a07da55cbfd469e32669b9f1525
Garrett Speed <6378547+gspeed0689@users.noreply.github.com>
2022-01-02 18:49:02 +0100

    more import commands

--- bin/mediaimport.py
+++ bin/mediaimport.py
@@ -1,17 +1,21 @@
+from video import Video
+from photo import Photo
 import config
 import os
 import time
 import datetime
 import multiprocessing
 import glob
+import shutil
 
 class Media_Import:
     def __init__(self, source_folder, output_folder="default", importdate="today"):
         self.source_folder = source_folder
         self.import_date = self.import_date(importdate)
-        self.output_folder = self.output_folder(output_folder)
-        self.staging_folder = self.staging_folder()
-        self.import_list = self.list_input_photos()
+        self.output_folder = self.determine_output_folder(output_folder)
+        self.staging_folder = self.determine_staging_folder()
+        self.import_list = self.list_input_media()
+        self.staging_media_list_evaluation()
         pass
     def import_date(self, passed_value):
         if config.timezone == "local":
@@ -26,7 +30,7 @@ class Media_Import:
                 yesterday = today - yesterday_timedelta
                 return_value = yesterday
         return(return_value)
-    def output_folder(self, passed_value):
+    def determine_output_folder(self, passed_value):
         if passed_value == "default":
             str_YM = config.folder_date_ym.replace("%Y", self.import_date.year).replace("%m", self.import_date.month).replace("%d", self.import_date.day)
             str_YMD = config.folder_date_ym.replace("%Y", self.import_date.year).replace("%m", self.import_date.month).replace("%d", self.import_date.day)
@@ -38,13 +42,13 @@ class Media_Import:
         else: 
             output_folder = passed_value
         return(output_folder)
-    def DC(self, fpath): #Directory Check and Create
-        if os.path.exists(fpath) == False:
-            os.mkdir(fpath)
-    def staging_folder(self):
+    def determine_staging_folder(self):
         staging_folder = self.output_folder + os.sep + "staging"
         self.DC(staging_folder)
         return(staging_folder)
+    def DC(self, fpath): #Directory Check and Create
+        if os.path.exists(fpath) == False:
+            os.mkdir(fpath)
     def list_input_media(self):
         media = {"all": []}
         for media_type in config.media_types.keys():
@@ -55,6 +59,19 @@ class Media_Import:
                 if len(glob_results) != 0:
                     media[file_type] = glob_results
                     media["all"] += glob_results
+        self.source_media = media
+    def move_media_staging(self):
+        for infile in self.source_media["all"]:
+            filename = infile.split(os.sep[-1])
+            outfile = self.staging_folder + os.sep + filename
+            shutil.copy2(infile, outfile)
+    def staging_media_list_evaluation(self):
+        self.staging_media = {key: [x.replace(self.source_folder, self.staging_folder) for x in value] for (key, value) in self.source_media.items()}
+        self.staging_videos = {key: [x.replace(self.source_folder, self.staging_folder) for x in value] for (key, value) in self.source_media.items() if key in config.media_types["videos"]}
+        self.staging_photos = {key: [x.replace(self.source_folder, self.staging_folder) for x in value] for (key, value) in self.source_media.items() if key in config.media_types["photos"]}
+        self.VIDEOS = {key: [Video(x) for x in value] for (key, value) in self.staging_videos}
+        self.PHOTOS = {key: [Photo(x) for x in value] for (key, value) in self.staging_photos}
+        pass
     def affinity_bin(self):
         total_cores = multiprocessing.cpu_count()
         active_cores = total_cores - config.affinity_reserve_cores
@@ -71,12 +88,26 @@ class Media_Import:
         return(return_list)
     def video_conversion_main(self):
         affinity_prefix = ["start", "/affinity", self.affinity_bin()]
+        cmd_list = self.video_commands()
+        cmd_list = [self.affinity_prefixing(affinity_prefix, x) for x in cmd_list]
+        pass
+    def video_commands(self):
+        videos = self.VIDEOS
+        all_commands = []
+        for video in videos:
+            out_filename_base = self.output_folder + os.sep + video.filename
+            for cmd in config.ffmpeg_video.keys():
+                new_cmd = [x.replace("{import_location}", video.filepath).replace("{output_location}", out_filename_base) for x in cmd]
+                all_commands.append(new_cmd)
+            pass
+        pass
+    def execute_video_conversion(self):
         if self.conversion == True:
             if self.conversion_method == "batch":
                 batch_file_location = self.output_folder + os.sep + "process_video.bat"
+                with open(batch_file_location, "w") as f:
+                    f.write("")
                 pass
             elif self.conversion_method == "subproc":
                 pass
-        pass
-    def execute_video_conversion(self):
         pass
--- bin/video.py
+++ bin/video.py
@@ -4,6 +4,8 @@ import os
 class Video:
     def __init__(self, file_path):
         self.filepath = file_path
-        self.filename = file_path.split(os.sep)[-1]
+        self.filename = file_path.split(os.sep)[-1].split(".")[0]
+        self.filenametype = file_path.split(os.sep)[-1]
         self.filetype = self.filename.split(".")[-1]
+        self.directory = os.path.dirname(file_path)
         pass       
